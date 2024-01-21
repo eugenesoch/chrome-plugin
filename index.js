dfebe79da5f8858ea8f6b1341e46267d05b72292
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-21 09:26:39 +0000

    More changes to the app

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const inputBtn = document.getElementById("input-btn")
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+// chrome://extensions/
+let myLeads = []
 const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
+localStorage.setItem("myLeads", "exampleleadm.com")
+
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" myLeads[i] "</li>"
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems +=    `
+                            <li>
+                                <a target='_blank' href='${myLeads[i]}'>
+                                    ${myLeads[i]}
+                                </a>
+                            </li>
+                        `
+    }
+    ulEl.innerHTML = listItems  
 }
