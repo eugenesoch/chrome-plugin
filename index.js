0b9fe26e0cc1df2ba1439f11c87e3ac2205f8f69
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-21 22:24:40 +0000

    Added delete button

--- index.js
+++ index.js
@@ -2,27 +2,43 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
-localStorage.setItem("myLeads", "exampleleadm.com")
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads =[]
+    renderLeads()
+});
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems +=    `
-                            <li>
-                                <a target='_blank' href='${myLeads[i]}'>
-                                    ${myLeads[i]}
-                                </a>
-                            </li>
-                        `
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems  
 }
